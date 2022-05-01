a99e39dbb3d6f608c5fa3e637e75359a4e12ce51
Ankur Acharya <ankur@youthinnovationlab.org>
2022-05-01 19:16:17 +0545

    New combined message
    branc 1 comit
    
    branc 1 comit 2
    
    branc 1 comit 3

--- file1.ts
+++ file1.ts
@@ -4,3 +4,7 @@ const commit1 = 'commit1 done';
 const commit2 = 'commit1 done';
 const commit3 = 'commit1 done';
 const commit4 = 'commit1 done';
+
+let b = 'some branch 1 commit';
+let c = 'some branch 1 commit 2';
+let d = 'some branch 1 commit 3';
